4e15bb07bd62ee86cc32ed2fb8fa70359a5b4b9b
Son Anh <buisonanh.study@gmail.com>
2024-10-02 20:34:04 +0700

    update cuda use for test code

--- test.py
+++ test.py
@@ -7,14 +7,16 @@ from data_aug.contrastive_learning_dataset import ContrastiveLearningDataset
 
 state_dict = torch.load("checkpoints\checkpoint_0002.pth.tar", map_location=torch.device('cpu'))
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-net = ResNetSimCLR(base_model='resnet18', out_dim=128)
-net.load_state_dict(state_dict['state_dict'])
+model = ResNetSimCLR(base_model='resnet18', out_dim=128)
+model.load_state_dict(state_dict['state_dict'])
 
-for param in net.parameters():
+for param in model.parameters():
     param.requires_grad = False
 
-model = nn.Sequential(net, nn.Linear(128, 7))
+net = nn.Sequential(model, nn.Linear(128, 7))
+net.to(device)
 
 trainset = torchvision.datasets.ImageFolder("fer_plus/train", transform=ContrastiveLearningDataset.get_simclr_pipeline_transform(224))
 testset = torchvision.datasets.ImageFolder("fer_plus/test", transform=ContrastiveLearningDataset.get_simclr_pipeline_transform(224))
@@ -23,26 +25,27 @@ train_loader = torch.utils.data.DataLoader(dataset=trainset, batch_size=128, shu
 test_loader = torch.utils.data.DataLoader(dataset=testset, batch_size=128, shuffle=False)
 
 
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
-
+optimizer = torch.optim.Adam(net.parameters(), lr=0.01, weight_decay=5e-4)
 
 for epoch in range(10):
-    model.train()
+    net.train()
     for i, (images, labels) in enumerate(train_loader):
         optimizer.zero_grad()
-        outputs = model(images)
+        images = images.to(device)
+        outputs = net(images)
         loss = nn.CrossEntropyLoss()(outputs, labels)
         loss.backward()
         optimizer.step()
         if i % 10 == 0:
             print(f"Epoch {epoch}, step {i}, loss {loss.item()}")
 
-    model.eval()
+    net.eval()
     correct = 0
     total = 0
     with torch.no_grad():
         for images, labels in test_loader:
-            outputs = model(images)
+            images = images.to(device)
+            outputs = net(images)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
